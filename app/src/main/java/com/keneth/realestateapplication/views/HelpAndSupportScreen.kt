2fe0a75385860afea2c1c4d9894700df7a4a48ee
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-02 20:54:43 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/HelpAndSupportScreen.kt
@@ -0,0 +1,9 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HelpAndSupportScreen(navController: NavController) {
+
+}
