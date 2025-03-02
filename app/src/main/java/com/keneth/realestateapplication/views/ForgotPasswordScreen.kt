10293309f53578d7701f13b0552b0f1050bfc33b
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-02 20:54:19 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/ForgotPasswordScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.keneth.realestateapplication.viewModels.UserViewModel
+
+@Composable
+fun ForgotPasswordScreen(navController: NavController, viewModelUser: UserViewModel) {
+
+}
