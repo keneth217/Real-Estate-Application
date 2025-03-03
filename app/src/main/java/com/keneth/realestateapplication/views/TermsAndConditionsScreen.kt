aec56adae752a2904dbd736e45b9ada5e1181a0f
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:15:22 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/TermsAndConditionsScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun TermsAndConditionsScreen(navController: NavController) {
+
+}
