e4a3bebe3ac2fad96eb1283bff4284fecf6ac729
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:11:00 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/NotificationsScreen.kt
@@ -0,0 +1,9 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun NotificationsScreen(navController: NavHostController) {
+
+}
