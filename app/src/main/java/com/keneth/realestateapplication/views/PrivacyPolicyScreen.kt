f3dc076d98cbd78373076a5fd344e07e7d50cc53
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:12:28 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/PrivacyPolicyScreen.kt
@@ -0,0 +1,9 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun PrivacyPolicyScreen(navController: NavHostController) {
+
+}
