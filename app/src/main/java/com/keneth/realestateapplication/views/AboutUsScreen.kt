bed4924307a7a25dedefc6de1215d72566209425
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:14:41 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/AboutUsScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun AboutUsScreen(navController: NavHostController) {
+
+}
