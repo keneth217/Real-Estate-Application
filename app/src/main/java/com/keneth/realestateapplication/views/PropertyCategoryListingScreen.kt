00234384ce55023b5bf4de0bac4ec1dd4ab6bb57
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:13:28 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/PropertyCategoryListingScreen.kt
@@ -0,0 +1,10 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.keneth.realestateapplication.viewModels.PropertyViewModel
+
+@Composable
+fun PropertyCategoryListingScreen(category: String, navController: NavController, viewModel: PropertyViewModel) {
+
+}
