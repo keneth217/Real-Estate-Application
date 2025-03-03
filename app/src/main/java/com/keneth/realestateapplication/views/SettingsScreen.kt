9def075a9681761b8bfde7ee492c92e2b04e2dc7
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-03 20:14:26 +0300

    Set up project,add firebase,screens and login functionality

---
+++ app/src/main/java/com/keneth/realestateapplication/views/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.keneth.realestateapplication.views
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun SettingsScreen(navController: NavHostController) {
+
+}
