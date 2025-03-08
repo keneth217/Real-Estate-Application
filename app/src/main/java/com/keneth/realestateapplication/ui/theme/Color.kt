fbd6cf487b2f737822b64fb2963ff18a4181a3b5
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-08 22:43:22 +0300

    add styles and fonts

--- app/src/main/java/com/keneth/realestateapplication/ui/theme/Color.kt
+++ app/src/main/java/com/keneth/realestateapplication/ui/theme/Color.kt
@@ -2,10 +2,30 @@ package com.keneth.realestateapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//light scheme
+val Green = Color(0xFF4CAF50)
+val White = Color(0xFFFFFFFF)
+val Pink = Color(0xFF4CAF50)
+val Black = Color(0xFF000000)
+val Red = Color(0xFFB00020)
+//dark
+val Light_Green = Color(0xFF81C784)
+val Light_Pink = Color(0xFFF48FB1)
+val Dark_Gray = Color(0xFF1E1E1E)
+val Light_Red = Color(0xFFCF6679)
+
+
+
+
+
+
+
+
