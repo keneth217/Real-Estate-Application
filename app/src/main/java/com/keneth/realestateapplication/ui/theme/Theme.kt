cf4166a6d4d59bdb1193676e9caa094dae88abe9
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-08 22:42:04 +0300

    add styles and fonts

--- app/src/main/java/com/keneth/realestateapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/keneth/realestateapplication/ui/theme/Theme.kt
@@ -12,31 +12,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Light_Green,
+    onPrimary = Black,
+    secondary = Light_Pink,
+    onSecondary = Black,
+    background = Dark_Gray,
+    onBackground = White,
+    surface = Dark_Gray,
+    onSurface = White,
+    error = Light_Red,
+    onError = Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Green,
+    onPrimary = White,
+    secondary = Pink,
+    onSecondary = White,
+    background = White,
+    onBackground = Black,
+    surface = White,
+    onSurface = Black,
+    error = Red,
+    onError = White
 )
 
 @Composable
 fun RealEstateApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -45,7 +48,6 @@ fun RealEstateApplicationTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
