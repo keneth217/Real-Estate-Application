6e43e8cd6201ad7c6abb5f589f355a117185aa96
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-09 22:19:59 +0300

    add  fonts ,update user profile

--- app/src/main/java/com/keneth/realestateapplication/enum/RegistrationStep.kt
+++ app/src/main/java/com/keneth/realestateapplication/enum/RegistrationStep.kt
@@ -3,6 +3,7 @@ package com.keneth.realestateapplication.enum
 enum class RegistrationStep {
     EMAIL_PASSWORD,
     PERSONAL_DETAILS,
+    USER_ROLES,
     ADDRESS,
     PROFILE_PICTURE,
     REVIEW
