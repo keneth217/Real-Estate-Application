b770d2d731f98d832e39781b809114060373ee87
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-13 16:52:30 +0300

    update dashboard

--- app/src/main/java/com/keneth/realestateapplication/views/SellPropertyScreen.kt
+++ app/src/main/java/com/keneth/realestateapplication/views/SellPropertyScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.keneth.realestateapplication.viewModels.PropertyViewModel
@@ -31,7 +32,7 @@ fun SellPropertyScreen(
                 title = "Mark as Sold",
                 onMenuClick = { navController.popBackStack() }
             )
-        }
+        }, containerColor = Color.White, contentColor = Color.Black,
     ) { paddingValues ->
         Column(
             modifier = Modifier
