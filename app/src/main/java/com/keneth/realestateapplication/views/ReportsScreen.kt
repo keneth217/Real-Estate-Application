6cb34dd61a68ef2fe66a24c24ce2a73ced64f769
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-15 21:45:06 +0300

    add user roles navigation and dashboard

--- app/src/main/java/com/keneth/realestateapplication/views/ReportsScreen.kt
+++ app/src/main/java/com/keneth/realestateapplication/views/ReportsScreen.kt
@@ -1,10 +1,37 @@
 package com.keneth.realestateapplication.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.keneth.realestateapplication.viewModels.PropertyViewModel
 
 @Composable
 fun ReportsScreen(navController: NavController, viewModel: PropertyViewModel) {
+    Scaffold(
+        topBar = {
+            Screen.Reports.title?.let {
+                AppTopBar(
+                    title = it,
+                    onMenuClick = { navController.popBackStack() }
+                )
+            }
+        }, containerColor = Color.White, contentColor = Color.Black
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Text(
+                text = "Reports Page"
+            )
 
+        }
+    }
 }
