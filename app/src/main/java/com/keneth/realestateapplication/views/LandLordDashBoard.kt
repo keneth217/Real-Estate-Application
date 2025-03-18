d322288389aa9897d36760482d236729bc5b18c4
KENETH217 <kipyegonkeneth03@gmail.com>
2025-03-18 21:46:58 +0300

    landlord screen and properties

--- app/src/main/java/com/keneth/realestateapplication/views/LandLordDashBoard.kt
+++ app/src/main/java/com/keneth/realestateapplication/views/LandLordDashBoard.kt
@@ -26,155 +26,130 @@ import java.time.LocalTime
 fun LandLordDashboardScreen(
     navController: NavHostController, viewModelUser: UserViewModel
 ) {
-    @Composable
-    fun LandLordDashboardScreen(
-        navController: NavHostController, viewModelUser: UserViewModel
-    ) {
-        val scope = rememberCoroutineScope()
-        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-        val currentRoute = navController.currentBackStackEntry?.destination?.route ?: ""
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val currentRoute = navController.currentBackStackEntry?.destination?.route ?: ""
 
-        var displayName by remember { mutableStateOf("User") }
-        val userDetails = viewModelUser.userProfile.value
-        val profileImage = viewModelUser.userProfile.value?.profileImage ?: ""
+    var displayName by remember { mutableStateOf("User") }
+    val userDetails = viewModelUser.userProfile.value
+    val profileImage = userDetails?.profileImage ?: ""
 
-        if (userDetails != null) {
-            displayName = userDetails.firstName.uppercase()
-        }
+    if (userDetails != null) {
+        displayName = userDetails.firstName.uppercase()
+    }
 
-        val currentHour = LocalTime.now().hour
-        val greeting = when (currentHour) {
+    val currentHour = LocalTime.now().hour
+    val greeting = remember {
+        when (currentHour) {
             in 0..11 -> "Good Morning"
             in 12..15 -> "Good Afternoon"
             in 16..20 -> "Good Evening"
             else -> "Good Night"
         }
+    }
 
-        LaunchedEffect(Unit) {
-            viewModelUser.fetchUserProfile()
-        }
+    LaunchedEffect(Unit) {
+        viewModelUser.fetchUserProfile()
+    }
 
-        ModalNavigationDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-                ModalDrawerSheet {
-                    AppDrawer(
-                        drawerState = drawerState,
-                        scope = scope,
-                        onItemClick = { route ->
-                            navController.navigate(route) { launchSingleTop = true }
-                            scope.launch { drawerState.close() }
-                        },
-                        onBackClick = { navController.popBackStack() },
-                        currentRoute = currentRoute,
-                        drawerItems = listOf(
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                AppDrawer(
+                    drawerState = drawerState,
+                    scope = scope,
+                    onItemClick = { route ->
+                        navController.navigate(route) { launchSingleTop = true }
+                        scope.launch { drawerState.close() }
+                    },
+                    onBackClick = { navController.popBackStack() },
+                    currentRoute = currentRoute,
+                    drawerItems = listOf(
+                        Screen.LandLordDashboard,
+                        Screen.AddProperty,
+                        Screen.LandLordPropertiesScreen
 
-                            Screen.LandLordDashboard,
-                            Screen.LandLordPropertiesScreen
-                        )
                     )
-                }
+                )
             }
-        ) {
-            Scaffold(
-                topBar = {
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                Screen.LandLordDashboard.title?.let {
                     AppTopBar(
-                        title = "Landlord Dashboard",
+                        title = it,
                         onMenuClick = { scope.launch { drawerState.open() } }
                     )
-                },
-                containerColor = Color.White,
-                contentColor = Color.Black
-            ) { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding)
-                        .padding(16.dp)
+                }
+            },
+            containerColor = Color.White,
+            contentColor = Color.Black
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp)
+            ) {
+                // Greeting and Profile Section
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    // Greeting and Profile Section
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Column {
-                            Text(
-                                text = "$greeting,",
-                                style = TextStyle(
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.Normal,
-                                    color = Color.Black
-                                )
+                    Column {
+                        Text(
+                            text = "$greeting,",
+                            style = TextStyle(fontSize = 20.sp, color = Color.Black)
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(
+                            text = displayName,
+                            style = TextStyle(fontSize = 20.sp,
+                                color = Color.Black,
+                                letterSpacing = 2.sp
+
                             )
-                            Spacer(modifier = Modifier.padding(top = 4.dp))
-                            Text(
-                                text = displayName,
-                                style = TextStyle(
-                                    fontSize = 30.sp,
-                                    fontWeight = FontWeight.Normal,
-                                    color = Color.Black,
-                                    letterSpacing = 2.0.sp
-                                )
-                            )
-                        }
-                        ProfilePicture(
-                            profilePicture = profileImage,
-                            profileImage = profileImage.toString(),
-                            modifier = Modifier
-                                .size(50.dp)
-                                .clip(CircleShape),
-                            navController = navController
                         )
                     }
+                    ProfilePicture(
+                        profilePicture = profileImage,
+                        profileImage = profileImage.toString(),
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(CircleShape),
+                        navController = navController
+                    )
+                }
 
-                    Text("Total Collections")
-                    Text("2,000,000.0 Kshs")
+                Text("Total Collections")
+                Text("2,000,000.0 Kshs")
 
-                    Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(20.dp))
 
-                    // Dashboard Cards in Two Columns
-                    Column {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            DashboardCard(
-                                title = "Total Properties",
-                                value = "10",
-                                modifier = Modifier.weight(1f)
-                            )
-                            DashboardCard(
-                                title = "Total Tenants",
-                                value = "25",
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            DashboardCard(
-                                title = "Pending Rent Payments",
-                                value = "$1,200",
-                                modifier = Modifier.weight(1f)
-                            )
-                            DashboardCard(
-                                title = "Maintenance Requests",
-                                value = "3",
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            DashboardCard(
-                                title = "Upcoming Lease Expirations",
-                                value = "2",
-                                modifier = Modifier.weight(1f)
-                            )
-                        }
+                // Dashboard Cards
+                Column {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        DashboardCard(title = "Total Properties", value = "10", modifier = Modifier.weight(1f))
+                        DashboardCard(title = "Total Tenants", value = "25", modifier = Modifier.weight(1f))
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        DashboardCard(title = "Pending Rent Payments", value = "$1,200", modifier = Modifier.weight(1f))
+                        DashboardCard(title = "Maintenance Requests", value = "3", modifier = Modifier.weight(1f))
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        DashboardCard(title = "Upcoming Lease Expirations", value = "2", modifier = Modifier.weight(1f))
                     }
                 }
             }
@@ -191,14 +166,10 @@ fun DashboardCard(title: String, value: String, modifier: Modifier = Modifier) {
     ) {
         Column {
             Text(text = title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
-            Text(
-                text = value,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Gray
-            )
+            Text(text = value, fontSize = 18.sp, fontWeight = FontWeight.Normal, color = Color.Gray)
         }
     }
 }
+
 
 
